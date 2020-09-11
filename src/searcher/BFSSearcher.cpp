be472ea5e02e966acbcfa52f6b62f9b4bf2d5d05
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-11 12:39:05 +0300

    Adding comments to BFS

--- src/searcher/BFSSearcher.cpp
+++ src/searcher/BFSSearcher.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 #include <map>
 #include <utility>
-#include <memory>
+#include <algorithm>
 
 namespace searcher {
 
@@ -29,33 +29,37 @@ namespace searcher {
             // dequeuing an element
             auto current = queue.pop();
 
+            // in case that the dequeued element is the end element, then finishing the search
             if (current == searchable.getEndElement()) {
-
+                // getting the optimal cost of the path, according the optimal paths that were calculated before
                 optimalCost = optimalPathInfo[current].first;
 
+                // iterating over the elements, and initializing the directions vector according to the optimal path
                 ElementType *temp = &current;
-
                 while (*temp != searchable.getStartElement()) {
-
+                    // adding a direction between two elements in the optimal path 
                     directions.insert(directions.begin(), searchable.getDirection(optimalPathInfo[*temp].second, *temp));
-
-                    temp = &optimalPathInfo[*temp].second;
+                    // moving to the previous element
+                    *temp = optimalPathInfo[*temp].second;
                 }
 
                 return SearchResult(directions, optimalCost, "BFS");
             }
 
             // getting all of the reachable elements of the dequeued element
-            // if one of them has not been visited, then adding it to the visited elements vector and enqueuing it
             for (auto reachable : searchable.getAllReachableElements(current)) {
+                // if the reachable element has not been visited, then adding it to the visited elements vector, 
+                // enqueuing it to the queue, and initializing its optimal path
                 if (std::find(visited.begin(), visited.end(), reachable) == visited.end()) {
-
-                    optimalPathInfo[reachable] = std::pair(reachable.getValue() + optimalPathInfo[current].first, current);
-
+                    visited.push_back(reachable);
                     queue.push(reachable);
+                    optimalPathInfo[reachable] = std::pair(reachable.getValue() + optimalPathInfo[current].first, current);
                 }
-                else if (reachable.getValue() + optimalPathInfo[current].first < optimalPathInfo[std::find(visited.begin(), visited.end(), reachable)].first) {
-                        optimalPathInfo[reachable] = std::pair(reachable.getValue() + optimalPathInfo[current].first, current);
+                // if the reachable element has been visited already, then checking if the path to this reachable element
+                // from the current element is more optimal than the current optimal path to the reachable, and if it is,
+                // then updating it accordingly 
+                else if (reachable.getValue() + optimalPathInfo[current].first < optimalPathInfo[reachable].first) {
+                    optimalPathInfo[reachable] = std::pair(reachable.getValue() + optimalPathInfo[current].first, current);
                 }
             }  
         }
