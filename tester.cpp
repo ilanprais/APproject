6bc369e8241de7c57234315848e3c1036b623f78
Ilan Prais <ilandovprais@gmail.com>
2020-09-18 17:57:08 +0300

    test

--- tester.cpp
+++ tester.cpp
@@ -7,21 +7,30 @@ using namespace searcher;
 
 int main() {
     // initializing a matrix
-    matrix::Matrix gMatrix(3, 3);
+    int k = 1;
+    matrix::Matrix gMatrix(5, 5);
     for (uint32_t i = 0; i < gMatrix.getHeight(); i++) {
         for (uint32_t j = 0; j < gMatrix.getWidth(); j++) {
-            gMatrix.setAt(i, j, 1);
+            if(i == 2 && j == 2){
+                gMatrix.setAt(i, j, 0);
+            }else{
+                gMatrix.setAt(i, j, k);
+            }
+            k++;
         }
     }
 
+    std::cout << gMatrix << std::endl;
+
+
     // creating a graph object from the matrix
-    Graph graph(gMatrix, std::pair<uint32_t, uint32_t>(0, 0), std::pair<uint32_t, uint32_t>(2, 2));
+    Graph graph(gMatrix, std::pair<uint32_t, uint32_t>(0, 0), std::pair<uint32_t, uint32_t>(4, 4));
 
     // creating a BFS searcher object
-    DFSSearcher<pair> DFS = DFSSearcher<pair>();
+    BFSSearcher<pair> BFS = BFSSearcher<pair>();
 
     // searching on the graph
-    SearchResult result = DFS.search(graph);
+    SearchResult result = BFS.search(graph);
 
     // printing the search result
     std::cout << result.toString();
