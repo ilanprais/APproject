c0b17fe4e800e548c640c5271f70af2c8e5f00d8
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-18 15:47:07 +0300

    Adding exceptions classes

---
+++ SearchExceptions.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace searcher {
+
+    namespace exceptions {
+
+        class InvalidStartPositionException : public std::runtime_error {
+
+            public:
+
+                InvalidStartPositionException();
+        };
+
+        class InvalidEndPositionException : public std::runtime_error {
+
+            public:
+
+                InvalidEndPositionException();
+        };
+    }
+}
