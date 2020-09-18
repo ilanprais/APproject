06958d10ac55efb0f85b0593a649675ecd06a021
Ilan Prais <ilandovprais@gmail.com>
2020-09-18 14:56:27 +0300

    Bfs search fix

--- BFSSearcher.hpp
+++ BFSSearcher.hpp
@@ -19,14 +19,11 @@ namespace searcher {
                 std::vector<ElementType> visited;
                 // this queue will be used for the BFS algorithm
                 std::queue<ElementType> queue;
-                // this map will hold for each element pair with the cost of the optimal path from the start element to the element
-                // and the previous element in this path
-                std::map<ElementType, std::pair<double, ElementType>> optimalPathsInfo;
+                // this map will hold for each element pair with the cost of the element and the previous element in this path
+                std::map<ElementType, std::pair<double, ElementType>> pathsInfo;
 
                 // this vector will hold the directions of the optimal way from the start element to the end element
                 std::vector<std::string> directions;
-                // the cost of the optimal path
-                double optimalCost = 0;
 
                 // adding the start element to the visited elements vector, enqueuing it, and adding it to the optimal paths map
                 visited.push_back(searchable.getStartElement());
@@ -41,11 +38,12 @@ namespace searcher {
                     // in case that the dequeued element is the end element, then finishing the search
                     if (current == searchable.getEndElement()) {
                         // getting the optimal cost of the path, according the optimal paths that were calculated before
-                        optimalCost = optimalPathsInfo.at(current).first;
+                        double cost = 0;
 
                         // iterating over the elements, and initializing the directions vector according to the optimal path
                         ElementType *temp = &current;
                         while (*temp != searchable.getStartElement()) {
+                            cost += optimalPathsInfo.at(*temp).first;
                             // adding a direction between two elements in the optimal path 
                             directions.insert(directions.begin(), searchable.getDirection(optimalPathsInfo.at(*temp).second, *temp));
                             // moving to the previous element
@@ -62,18 +60,10 @@ namespace searcher {
                         if (std::find(visited.begin(), visited.end(), reachable) == visited.end()) {
                             visited.push_back(reachable);
                             queue.push(reachable);
-                            optimalPathsInfo.emplace(reachable, std::make_pair(reachable.getValue() + optimalPathsInfo.at(current).first, current));
-                        }
-                        // if the reachable element has been visited already, then checking if the path to this reachable element
-                        // from the current element is more optimal than the current optimal path to the reachable, and if it is,
-                        // then updating it accordingly 
-                        else if (reachable.getValue() + optimalPathsInfo.at(current).first < optimalPathsInfo.at(reachable).first) {
-                            optimalPathsInfo.emplace(reachable, std::make_pair(reachable.getValue() + optimalPathsInfo.at(current).first, current));
+                            pathsInfo.emplace(reachable, std::make_pair(reachable.getValue(), current));
                         }
                     }  
                 }
-
-                //return SearchResult(directions, optimalCost, "BFS");
             }
     };
 }
