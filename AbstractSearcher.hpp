9447e7cd86f73609dc1fde815d6bd01488b133a9
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-19 20:52:59 +0300

    Adding AbstractSearcher class

---
+++ AbstractSearcher.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Searcher.hpp"
+#include <map>
+
+namespace searcher {
+
+    template <typename SearchResultType, typename Identifier>
+    class AbstractSearcher : public Searcher<SearchResultType, Identifier> {
+
+        public:
+
+            virtual SearchResultType search(const Searchable<Identifier>& searchable) const = 0;
+
+        protected:
+
+            virtual SearchResult reconstructPath(const Searchable<Identifier>& searchable, 
+                std::map<Element<Identifier>, Element<Identifier>, CompareByIdentifier<Identifier>>& cameFrom) const {
+                // this vector will hold the directions of the path from the start element to the end element
+                std::vector<std::string> directions;
+
+                // this variable will hold the total cost of the path. 
+                // first, initializing it just with the cost of the start element
+                auto pathCost = searchable.getStartElement().getValue();
+
+                // iterating over the elements, and initializing the directions vector according to the path
+                const auto *temp = &searchable.getEndElement();
+                while (*temp != searchable.getStartElement()) {
+                    // adding the cost of the current element to the total cost of the path
+                    pathCost += temp->getValue();
+                    // adding a direction between two elements in the path 
+                    directions.insert(directions.begin(), searchable.getDirection(cameFrom.at(*temp), *temp));
+                    // moving to the previous element
+                    temp = &cameFrom.at(*temp);
+                }
+
+                return SearchResult(directions, pathCost, "BFS");
+            }
+    };
+}
--- BFSSearcher.hpp
+++ BFSSearcher.hpp
@@ -4,14 +4,13 @@
 #include "SearchResult.hpp"
 #include "SearchExceptions.hpp"
 #include <queue>
-#include <map>
 #include <utility>
 #include <algorithm>
 
 namespace searcher {
 
     template <typename Identifier>
-    class BFSSearcher : public Searcher<SearchResult, Identifier> {
+    class BFSSearcher : public AbstractSearcher<SearchResult, Identifier> {
 
         public:
 
@@ -55,56 +54,6 @@ namespace searcher {
                 if (std::find(visited.begin(), visited.end(), searchable.getEndElement()) == visited.end()) {
                     throw exceptions::PathDoesNotExistException();
                 }
-            }
-
-        private:
-
-            SearchResult reconstructPath(const Searchable<Identifier>& searchable, 
-                std::map<Element<Identifier>, Element<Identifier>, CompareByIdentifier<Identifier>>& cameFrom) const {
-                    // this vector will hold the directions of the path from the start element to the end element
-                    std::vector<std::string> directions;
-
-                    // this variable will hold the total cost of the path. 
-                    // first, initializing it just with the cost of the start element
-                    auto pathCost = searchable.getStartElement().getValue();
-
-                    // iterating over the elements, and initializing the directions vector according to the path
-                    const auto *temp = &searchable.getEndElement();
-                    while (*temp != searchable.getStartElement()) {
-                        // adding the cost of the current element to the total cost of the path
-                        pathCost += temp->getValue();
-                        // adding a direction between two elements in the path 
-                        directions.insert(directions.begin(), searchable.getDirection(cameFrom.at(*temp), *temp));
-                        // moving to the previous element
-                        temp = &cameFrom.at(*temp);
-                    }
-
-                    return SearchResult(directions, pathCost, "BFS");
-            }
-
-        private:
-
-            SearchResult reconstructPath(const Searchable<Identifier>& searchable, 
-                std::map<Element<Identifier>, Element<Identifier>, CompareByIdentifier<Identifier>>& cameFrom) const {
-                    // this vector will hold the directions of the path from the start element to the end element
-                    std::vector<std::string> directions;
-
-                    // this variable will hold the total cost of the path. 
-                    // first, initializing it just with the cost of the start element
-                    auto pathCost = searchable.getStartElement().getValue();
-
-                    // iterating over the elements, and initializing the directions vector according to the path
-                    const auto *temp = &searchable.getEndElement();
-                    while (*temp != searchable.getStartElement()) {
-                        // adding the cost of the current element to the total cost of the path
-                        pathCost += temp->getValue();
-                        // adding a direction between two elements in the path 
-                        directions.insert(directions.begin(), searchable.getDirection(cameFrom.at(*temp), *temp));
-                        // moving to the previous element
-                        temp = &cameFrom.at(*temp);
-                    }
-
-                    return SearchResult(directions, pathCost, "BFS");
             }
     };
 }
